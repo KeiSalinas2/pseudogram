e371e45206abacd01ad6eaf3be413bc50d6c6fce
Keily Salinas <keisalinas2@gmail.com>
2017-02-19 18:15:00 -0400

    add firebase config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+import config from '../config/config'
 import App from './App';
 import './index.css';
+
+console.log(config);
+
+firebase.initializeApp({
+  apiKey: config.firebase.apiKey,
+  authDomain: config.firebase.authDomain,
+  databaseURL: config.firebase.databaseURL,
+  storageBucket: config.firebase.storageBucket,
+  messagingSenderId: config.firebase.messagingSenderId
+});
 
 ReactDOM.render(
   <App />,
